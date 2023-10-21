c3205eadca9de4bfccb14b4ae1067958a722ad4f
Mohamed1492 <sellami.med.tn@gmail.com>
2023-10-21 11:50:32 +0100

    update days of work

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, "public")))
 
 function Logger(req, res, next){
 
-    if ((new Date().getDay()>0) && (new Date().getDay()<7) && (new Date().getHours()>8) && (new Date().getHours()<18)){
+    if ((new Date().getDay()>0) && (new Date().getDay()<6) && (new Date().getHours()>8) && (new Date().getHours()<18)){
     next()}
     else{
         res.send(` <h1>Sorry, this site is currently closed.</h1> <br> <h1>Please come back during working hours (Monday to Friday,  from 9 to 17)</h1>`)
